4252c2fafc1fdbb80acf52661a60de4a26c49192
qkralsun480 <93891993+qkralsun480@users.noreply.github.com>
2022-02-06 11:44:17 +0900

    Update 거리두기.py

--- programmer/거리두기.py
+++ programmer/거리두기.py
@@ -1,62 +1,47 @@
 from collections import deque
 
-def bfs(arr_list):
-    people_location = []
-    for i in range(5):
-        for j in range(5):
-            if arr_list[i][j] == 'P':
-                people_location.append([i,j])
-    print(people_location)
+def bfs(p):
+    start = []
     
-    for data in people_location:
-        queue = deque([data])
+    for i in range(5): # 시작점이 되는 P 좌표 구하기
+        for j in range(5):
+            if p[i][j] == 'P':
+                start.append([i, j])
+    
+    for s in start:
+        queue = deque([s])  # 큐에 초기값
         visited = [[0]*5 for i in range(5)]   # 방문 처리 리스트
-        print(visited)
         distance = [[0]*5 for i in range(5)]  # 경로 길이 리스트
-        visited[data[0]][data[1]] = 1
+        visited[s[0]][s[1]] = 1
         
         while queue:
-            x,y = queue.popleft() 
+            y, x = queue.popleft()
+        
+            dx = [-1, 1, 0, 0]  # 좌우
+            dy = [0, 0, -1, 1]  # 상하
 
-            dx = [1,-1,0,0]
-            dy = [0,0,-1,1]
-            
             for i in range(4):
                 nx = x + dx[i]
                 ny = y + dy[i]
-                ## 이부분을 생각해야 할듯
+
                 if 0<=nx<5 and 0<=ny<5 and visited[ny][nx] == 0:
-                    if arr_list[nx][ny] == 'O':
-                        queue.append([nx][ny])
-                        visited[nx][ny] = 1
-                        distance[nx][ny] = distance[x][y]+1
-                    if arr_list[nx][ny] == 'P' and distance[x][y]<=1:
+                    
+                    if p[ny][nx] == 'O':
+                        queue.append([ny, nx])
+                        visited[ny][nx] = 1
+                        distance[ny][nx] = distance[y][x] + 1
+                    
+                    if p[ny][nx] == 'P' and distance[y][x] <= 1:
                         return 0
+                    if p[ny][nx] == 'X':
+                        visited[ny][nx] = 1
     return 1
-                          
-                      
-                        
-    
+
+
 def solution(places):
     answer = []
     
     for i in places:
-        answer.append(bfs(i))  
+        answer.append(bfs(i))
+    
     return answer
-
-def main():
-    places = [
-    ["POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"],
-    ["POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"], 
-    ["PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"],
-    ["OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"],
-    ["PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"]
-    ]
-    result = solution(places)
-    print(result)
-
-if __name__ == "__main__":
-	main()
-'''
-[1, 0, 1, 1, 1'
-'''
